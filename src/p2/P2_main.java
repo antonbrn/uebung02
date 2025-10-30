7fc231788c71d694d05a4b97334584ae9d3ae9ae
antonbaron <a_baron11@icloud.com>
2025-10-30 10:28:28 +0100

    Präsenzaufgabe2

---
+++ src/p2/P2_main.java
@@ -0,0 +1,14 @@
+package p2;
+
+public class P2_main {
+
+	static int i = 234;
+	static boolean b = false;
+	
+	public static void main(String[] args) {
+		if(i > 10) {
+			b = true;
+		}
+		System.out.println(b);
+	}
+}
